9126a5fc390a74cfe0bcde372cacea2cd13db52c
Laboratório FIAP <labsfiap@fiap.com.br>
2025-10-20 21:45:15 -0300

    Trabalhando com cards

--- app/src/main/java/guilherme26bc/com/github/android_imc_app/MainActivity.kt
+++ app/src/main/java/guilherme26bc/com/github/android_imc_app/MainActivity.kt
@@ -5,21 +5,35 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -56,6 +70,7 @@ fun IMCScreen(modifier: Modifier = Modifier) {
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(160.dp)
+                    .background(colorResource(id = R.color.vermelho_fiap))
             ) {
                 Image(
                     painter = painterResource(id = R.drawable.bmi),
@@ -78,6 +93,94 @@ fun IMCScreen(modifier: Modifier = Modifier) {
                     .fillMaxWidth()
                     .padding(horizontal = 32.dp)
             ) {
+                Card(
+                    modifier = Modifier
+                        .offset(y = (-30).dp)
+                        .fillMaxWidth()
+                        .height(300.dp),
+                    colors = CardDefaults
+                        .cardColors(containerColor = Color(0xfff9f6f6)),
+                    elevation = CardDefaults.cardElevation(4.dp)
+                ) {
+
+                    Text(
+                        text = "Seus dados",
+                        modifier = Modifier.fillMaxWidth(),
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = colorResource(id = R.color.vermelho_fiap),
+                        textAlign = TextAlign.Center
+                    )
+                    Spacer(modifier = Modifier.height(32.dp))
+                    Text(
+                        text = "Seu peso",
+                        modifier = Modifier.padding(bottom = 8.dp),
+                        fontSize = 12.sp,
+                        fontWeight = FontWeight.Normal,
+                        color = colorResource(id = R.color.vermelho_fiap)
+                    )
+                    OutlinedTextField(
+                        value = "",
+                        onValueChange = {},
+                        modifier = Modifier.fillMaxWidth(),
+                        placeholder = {
+                            Text(text = "Seu peso em Kg.")
+                        },
+                        colors = OutlinedTextFieldDefaults.colors(
+                            unfocusedBorderColor = colorResource(id = R.color.vermelho_fiap),
+                            focusedBorderColor = colorResource(id = R.color.vermelho_fiap)
+                        ),
+                        shape = RoundedCornerShape(16.dp),
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Text(
+                        text = "Sua altura",
+                        modifier = Modifier.padding(bottom = 8.dp),
+                        fontSize = 12.sp,
+                        fontWeight = FontWeight.Normal,
+                        color = colorResource(id = R.color.vermelho_fiap)
+                    )
+                    OutlinedTextField(
+                        value = "",
+                        onValueChange = {},
+                        modifier = Modifier.fillMaxWidth(),
+                        placeholder = {
+                            Text(
+                                text = "Sua altura em cm."
+                            )
+                        },
+                        colors = OutlinedTextFieldDefaults.colors(
+                            unfocusedBorderColor = colorResource(id = R.color.vermelho_fiap),
+                            focusedBorderColor = colorResource(id = R.color.vermelho_fiap)
+                        ),
+                        shape = RoundedCornerShape(16.dp),
+                        keyboardOptions = KeyboardOptions(
+                            keyboardType =
+                                KeyboardType.Decimal
+                        )
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Button(
+                        onClick = {},
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(48.dp),
+                        shape = RoundedCornerShape(16.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor =
+                                colorResource(id = R.color.vermelho_fiap)
+                        )
+                    ) {
+                        Text(
+                            text = "CALCULAR",
+                            fontWeight = FontWeight.Bold,
+                            color = Color.White,
+                            fontSize = 14.sp
+                        )
+                    }
+
+                }
             }
         }
         // -- Card Resultado

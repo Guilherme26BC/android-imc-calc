23df37a6060159c6700251cf0182fc1822626a53
Laboratório FIAP <labsfiap@fiap.com.br>
2025-10-20 21:34:07 -0300

    incluindo image e arquivo colors

--- app/src/main/java/guilherme26bc/com/github/android_imc_app/MainActivity.kt
+++ app/src/main/java/guilherme26bc/com/github/android_imc_app/MainActivity.kt
@@ -4,19 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import guilherme26bc.com.github.android_imc_app.ui.theme.AndroidimcappTheme
 
 class MainActivity : ComponentActivity() {
@@ -51,6 +57,20 @@ fun IMCScreen(modifier: Modifier = Modifier) {
                     .fillMaxWidth()
                     .height(160.dp)
             ) {
+                Image(
+                    painter = painterResource(id = R.drawable.bmi),
+                    contentDescription = "logo",
+                    modifier = Modifier
+                        .size(100.dp)
+                        .padding(top = 16.dp)
+                )
+                Text(
+                    text = "Calculadora IMC",
+                    fontSize = 24.sp,
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(top = 12.dp, bottom = 24.dp)
+                )
             }
             // --- formulário
             Column(
